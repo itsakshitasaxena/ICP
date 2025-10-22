72649789e5dde7ff4137159dcfaf5358fbef9a34
Akshita Saxena <166048233+itsakshitasaxena@users.noreply.github.com>
2025-10-23 01:35:14 +0530

    Add files via upload

---
+++ Day10_Assignment/Car_Fleet.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int[][] ar=new int[speed.length][2];
+        for(int i=0;i<speed.length;i++){
+            ar[i][0]=position[i];
+            ar[i][1]=speed[i];
+        }
+        Arrays.sort(ar,(a,b)->a[0]-b[0]);
+        Stack<Double> st=new Stack<>();
+        for(int[] a:ar){
+            if(a[0]>target)  continue;
+            double fleet=(target-a[0])/(1.0*a[1]);
+            while(!st.isEmpty() && st.peek()<=fleet)
+                st.pop();
+            st.push(fleet);
+        }
+        return st.size();
+    }
+}
---
+++ Day10_Assignment/Implement_Stack_using_Queues.java
@@ -0,0 +1,44 @@
+class MyStack {
+    Queue<Integer> q;
+    Queue<Integer> q1;
+    public MyStack() {
+        q=new LinkedList<>();
+        q1=new LinkedList<>();
+    }
+    
+    public void push(int x) {
+       if(q.isEmpty()){
+            q.add(x);
+        }
+       else{
+            while(!q.isEmpty()){
+                q1.add(q.poll());
+            }
+            q.add(x);
+            while(!q1.isEmpty()){
+                q.add(q1.poll());
+            }
+       }
+    }
+    
+    public int pop() {
+        return q.poll();
+    }
+    
+    public int top() {
+        return q.peek();
+    }
+    
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
